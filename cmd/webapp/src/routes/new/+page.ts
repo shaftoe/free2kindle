3ffd94ba5bd3345c200b4cd3e3e47835102ebc9f
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-02-12 16:46:48 -0400

    web: fix /new redirect

--- cmd/webapp/src/routes/new/+page.ts
+++ cmd/webapp/src/routes/new/+page.ts
@@ -1,8 +1,20 @@
+import { getToken } from "$lib/stores/token";
+import { redirect } from "@sveltejs/kit";
+import { browser } from "$app/environment";
+
 export async function load({
   parent,
 }: {
   parent: () => Promise<{ apiUrl: string }>;
 }): Promise<{ apiUrl: string }> {
+  const token = getToken();
+
+  if (!token) {
+    if (browser) {
+      redirect(302, "/settings");
+    }
+  }
+
   const { apiUrl } = await parent();
   return { apiUrl };
 }
